e054f14f3d7ad6075c5e244b7ab24f0ab5635b1c
Seoyule Jo <seoyule@gmail.com>
2023-12-28 13:18:43 +0900

    edit

--- script.js
+++ script.js
@@ -43,16 +43,25 @@ button.addEventListener('click', () => {
 
     if(t_value_status && num_m_status && num_w_status && dc_rate_status){
 
-        const m_fee_r = t_value*1.04/(num_m + (num_w*(1-dc_rate)));
+        const m_fee_r = t_value/(num_m + (num_w*(1-dc_rate)));
         const w_fee_r = m_fee_r*(1-dc_rate);
 
-        const m_fee = Math.ceil(m_fee_r/100)*100;
-        const w_fee = Math.ceil(w_fee_r/100)*100;
-        const acc = m_fee*num_m +w_fee*(num_w)-t_value;
+        const m_fee = Math.ceil(m_fee_r/1000)*1000;
+        const w_fee = Math.ceil(w_fee_r/1000)*1000;
 
-        result1.innerHTML = '남성 회비    : ' + m_fee + '원/인당';
-        result2.innerHTML = '여성+벙주 회비: ' + w_fee + '원/인당';
-        result3.innerHTML = '적립금      : ' + acc + '원 (4%), <br>계좌번호: 우리은행 1002 959 801068 박미영';
+        if(num_m == '0'){
+            result1.innerHTML = '남성 회비    : 0원/인당';
+        }else{
+            result1.innerHTML = '남성 회비    : ' + m_fee + '원/인당';
+        }
+
+        if(num_w == '0'){
+            result2.innerHTML = '여성+벙주 회비: 0원/인당';
+        }else{
+            result2.innerHTML = '여성+벙주 회비: ' + w_fee + '원/인당';
+        }
+
+        result3.innerHTML = '적립금입금 계좌번호: 우리은행(박미영)<br>1002 959 801068';
 
 
     }else{
@@ -71,7 +80,6 @@ button2.addEventListener('click', () => {
     const t_value = document.getElementById('t_value');
     const num_m = document.getElementById('num_m');
     const num_w = document.getElementById('num_w');
-    const dc_rate = document.getElementById('dc_rate');
 
     const result1 = document.getElementById('output_m_fee');
     const result2 = document.getElementById('output_w_fee');
@@ -80,6 +88,7 @@ button2.addEventListener('click', () => {
     t_value.value = '';
     num_m.value = '';
     num_w.value = '';
+
     result1.innerHTML = '';
     result2.innerHTML = '';
     result3.innerHTML = '';
